af33a915060e08abfa20c5614219839c0b0332cc
Huy0811 <duongchihuy.study@gmail.com>
2024-08-05 21:19:20 +0700

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,47 @@
-export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page")
+import { HeroSection } from "@/components/custom/HeroSection"
+import { flattenAttributes } from "@/lib/utils"
+import qs from "qs"
 
-  const { title, description } = strapiData.data.attributes
-
-  return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
-    </main>
-  );
-}
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"]
+        },
+        link: {
+          populate: true
+        }
+      }
+    }
+  }
+})
 
 async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337"
 
+  const url = new URL(path, baseUrl)
+  url.search = homePageQuery
+
   try {
-    const response = await fetch(baseUrl + path)
+    const response = await fetch(url.href, { cache: "no-store" })
     const data = await response.json()
-    return data
+    const flattenedData = flattenAttributes(data)
+
+    return flattenedData
   } catch (error) {
     console.error(error)
   }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page")
+
+  const { title, description, blocks } = strapiData
+
+  return (
+    <main>
+      <HeroSection data={blocks[0]} />
+    </main>
+  )
 }
